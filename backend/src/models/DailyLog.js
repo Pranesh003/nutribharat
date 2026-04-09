cc73d9cc2c7b3f1255f71d7df71c6cc1f7abb774
pranesh k <145831392+Pranesh003@users.noreply.github.com>
2026-04-09 23:05:14 +0530

    Remove empty line in DailyLog model
    Removed an empty line at the beginning of the DailyLog model.

--- backend/src/models/DailyLog.js
+++ backend/src/models/DailyLog.js
@@ -1,7 +1,6 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
 
-
 const DailyLog = sequelize.define('DailyLog', {
     id: {
         type: DataTypes.INTEGER,
