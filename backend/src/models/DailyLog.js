92a5e5ff7a50c561eb11f9f81722860137036727
pranesh k <145831392+Pranesh003@users.noreply.github.com>
2026-04-09 23:05:02 +0530

    Update DailyLog.js

--- backend/src/models/DailyLog.js
+++ backend/src/models/DailyLog.js
@@ -1,6 +1,7 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
 
+
 const DailyLog = sequelize.define('DailyLog', {
     id: {
         type: DataTypes.INTEGER,
